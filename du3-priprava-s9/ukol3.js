ead7c205e124dde973df0c2161a2ae575180e56c
Hong Anh Tranova <Tranhaova0123@gmail.com>
2020-07-15 22:34:32 +0200

    Domácí úkol bez hardcore úkolu

--- du3-priprava-s9/ukol3.js
+++ du3-priprava-s9/ukol3.js
@@ -3,14 +3,14 @@ function generator() {
   var pole = []
   var a =0;
   var b =0;
-  var c =0;
+  var c =0.0;
   if (q < 0.33) {
     //neni trojuhlenik
     pole[0] = false;
     pole[1] = "ne";
     a = Math.round(Math.random()*90) + 10;
     b = Math.round(Math.random()*90) + 10;
-    c = a+b + Math.round(Math.random()*10);
+    c = a+b + Math.round(Math.random()*10+1);
   }
   else if (q < 0.66) {
     //nepravouhly trojuhlenik
@@ -20,7 +20,7 @@ function generator() {
     b = Math.round(Math.random()*90) + 10;
     gama = 90;
     while (gama==90) {
-      gama = Math.round(Math.random()*178) +1;
+      gama = (Math.round(Math.random()*178) +1)/360*2*Math.PI;
     }
     c = Math.sqrt(a**2 + b**2 - a*b*Math.cos(gama));
   }
@@ -57,6 +57,7 @@ function generator() {
 }
 function kontrola(zadani, je_trojuhlenik, je_pravouhly) {
   //Trojuhelnik
+  console.log("a="+zadani[2]+"\n b="+zadani[3] + "\n c="+zadani[4]);
   if (zadani[0]== je_trojuhlenik) {
     console.log("Rozpoznání trojúhelníkovosti úspěšné! Tvoje správné řešení: " + je_trojuhlenik);
   }
@@ -73,8 +74,61 @@ function kontrola(zadani, je_trojuhlenik, je_pravouhly) {
 console.log("Snad to funguje");
 }
 zadani = generator();
-a = zadani[2];
-b = zadani[3];
-c = zadani[4];
+var a = zadani[2];
+var b = zadani[3];
+var c = zadani[4];
 
-kontrola(zadani, true, "a")
+function trojuhelnik(a,b,c){
+  var  vratit = 5;
+  if (a+b>c && a+c>b && b+c>a) {
+  console.log("Hodnoty tvoří trojúhelník");
+    vratit = true;
+  }
+  else {
+    console.log("Hodnoty netvoří trojúhelník");
+  vratit = false;
+  }
+  return vratit;
+}
+
+function pravouhly(a,b,c){
+  var vraceni = 0;
+  var nejvetsi = 0;
+  if (a**2 + b**2 == c**2|| a**2 + c**2 == b**2 || b**2 + c**2 == a**2) {
+  if (a>b && a>c) {
+    vraceni = "a";
+  }
+  else if (b>a && b>c) {
+    vraceni = "b";
+  }
+  else if(c>a&&c>b){
+    vraceni = "c";
+  }
+  console.log("trojúhelník je pravoúhlý");
+  }
+  else {
+    console.log("trojúhelník není pravoúhlý");
+  vraceni = "ne";}
+  return vraceni;
+  }
+kontrola(zadani, trojuhelnik(a,b,c), pravouhly(a,b,c));
+//ukazka vstupu funkce a vraceni hodnot
+function testovaci(cislo){
+  var vraceni = true;
+  if(cislo>5){
+    vraceni = true;
+  }
+  else {
+    vraceni = false;
+  }
+  return vraceni;
+}
+if (testovaci(8)) {
+  console.log("8 splňuje podmínky");
+}
+if (testovaci(3)) {
+  console.log("Splňuje");
+}
+else {
+  console.log("Nesplňuje!");
+}
--- du3-priprava-s9/ukoly.js
+++ du3-priprava-s9/ukoly.js
@@ -52,3 +52,49 @@ for (var k = Math.round(Math.random()*10000); k > 12; k=k) {
     k = k + 3;
   }
 }
+
+//úkol 3
+function trojuhelnik(a,b,c){
+  var  vratit = 5;
+  if (a+b>c && a+c>b && b+c>a) {
+  console.log("Hodnoty tvoří trojúhelník");
+    vratit = true;
+  }
+  else {
+    console.log("Hodnoty netvoří trojúhelník");
+  vratit = false;
+  }
+  return vratit;
+}
+
+
+
+
+
+
+function pravouhly(a,b,c){
+  var vraceni = 0;
+  var nejvetsi = 0;
+  if (a**2 + b**2 == c^**2|| a**2 + c**2 == b**2 || b**2 + c**2 == a**2) {
+  if (a>b && a>c) {
+    vraceni = "a";
+  }
+  else if (b>a && b>c) {
+    vraceni = "b";
+  }
+  else if(c>a&&c>b){
+    vraceni = "c";
+  }
+  console.log("trojúhelník je pravoúhlý");
+  }
+  else {
+    console.log("trojúhelník není pravoúhlý");
+  vraceni = "ne";}
+  return vraceni;
+  }
+
+
+//úkol 4
+var x = 0;
+var y = 0;
+var z = 0;
